156bbc854e5da2c6870c2805a44ffa9384aabd3a
Jéssica Nathany <jessicanathany.web@gmail.com>
2023-03-30 09:46:43 -0300

    finalização da api proficiendia dev, fazendo uma chamada post no openai

--- proficiencia.dev.api/controller/app.py
+++ proficiencia.dev.api/controller/app.py
@@ -1,39 +1,29 @@
-# Flask é um framework para criar aplicações web com Python
-# 1. objetivo - criar uma api que faça uam consulta no endpoint do openai, fazendo uma pergunta e obtendo resposta ao chatgpt
-# 2. url base - localhost.com 
-# 3. endpoints 
-#   - localhost/app/pergunta (POST)
-# 4. quais recursos - POST (para fazer uma pergunta ao chatgpt)
-
-# importando biblioteca do Flask
 from flask import Flask, jsonify, request
+import requests
 
 app = Flask(__name__)
 
-pergunta =  {'Quais são as linguagens de programação mais usadas no backend?'}
-
 # faz a pergunta ao chatgpt
 @app.route('/api/pergunta', methods=['POST'])
-def perguntar(pergunta):
+def pergunta():
+    obem_pergunta = request.get_json()
+    pergunta = obem_pergunta['pergunta']
     url = 'https://api.openai.com/v1/completions'
 
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer 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',
+        'Authorization': 'Bearer API-KEY',
     }
 
     data = {
         'prompt': pergunta,
-        'max_tokens': 7,
-        'temperature': 0,
+        'max_tokens': 3000,
         'model' : 'text-davinci-003',
-        'stream': False,
-        'logprobs': None,
-        'stop': '\n'
     }
 
-    response = request.post(url, headers=headers, json=data)
+    response = requests.post(url, headers=headers, json=data)
     return response.json()
+
     
-    
-app.run(port=5000, host='localhost', debug=True)
+if __name__ == '__main__':
+    app.run(port=5000, host='localhost', debug=True)
--- proficiencia.dev.api/controller/config.py
+++
@@ -1,8 +0,0 @@
-import os
-import openai
-import requests
-
-API_KEY = "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"
-OPENAI_MODEL = "davinci" 
-OPENAI_ENDPOINT = 'https://api.openai.com/v1/completions'
-OPENAI_ORGANIZATION = "org-98xQeEtUyTqDscpMWu5NsWi5" 
